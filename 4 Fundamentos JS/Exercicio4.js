74b684df527325100fa0b8c09c56de366f719e4e
Your Name pa-freire]git config user.email <you@example.com>
2024-07-23 19:30:31 -0300

    atualização

--- 4 Fundamentos JS/Exercicio4.js
+++ 4 Fundamentos JS/Exercicio4.js
@@ -1,10 +1,23 @@
-alert("Bem-vindo ao JavaScript")
+// alert("Bem-vindo ao JavaScript");
 
 var nome = "João";
 let idade = 25;
 const ano = 2024;
 
-var soma = idade + 5{
-    alert("soma")
+function somaNumero(num1, num2){
+    return num1 + num2;
 }
 
+let resultado = somaNumero(5, 3);
+console.log(resultado);
+// alert("5 + 3")
+// alert(resultado)
+
+function parImpar(idade){
+if(25 % 2 ===  0){
+    console.log(idade + " é par");
+}else{
+    console.log(Idade + " é impar");
+}
+}
+parImpar(idade);
