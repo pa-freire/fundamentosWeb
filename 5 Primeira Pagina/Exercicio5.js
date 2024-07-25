c7dc9fb039c1de9474a39f084739973c151059fc
Your Name pa-freire]git config user.email <you@example.com>
2024-07-24 23:36:28 -0300

    atualização

---
+++ 5 Primeira Pagina/Exercicio5.js
@@ -0,0 +1,7 @@
+document.getElementById('alertButton').addEventListener('click', function(){alert('Você clicou no botão')
+});
+
+var elemento = document.getElementById('bem');
+elemento.style.color = 'red';
+
+alert("Bem-vindo a minha página");
